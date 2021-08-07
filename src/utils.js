4171383bbf8173d40e0489df51d84ff9114418f4
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2021-08-07 14:10:45 +0300

    Добавил функции отрисовки шаблонов и элементов

--- src/utils.js
+++ src/utils.js
@@ -7,3 +7,29 @@ export const getRandomNumberInRange = (min = 0, max = 1, numberSymbolsAfterComma
   const randomNumber = Math.random() * (upper - lower) + lower;
   return +randomNumber.toFixed(numberSymbolsAfterComma);
 };
+
+export const RenderPosition = {
+  AFTERBEGIN: 'afterbegin',
+  BEFOREEND: 'beforeend',
+};
+
+export const renderElement = (container, element, place) => {
+  switch (place) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+  }
+};
+
+export const renderTemplate = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+export const createElement = (template) => {
+  const newElement = document.createElement('div');
+  newElement.innerHTML = template;
+  return newElement.firstChild;
+};
