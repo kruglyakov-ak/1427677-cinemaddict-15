ff8c4ca92bd13f0eed5cb0d4522c8af229052bc9
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2021-08-09 13:48:58 +0300

    Добавил сообщение при отсутствии фильмов

--- src/view/films-list.js
+++ src/view/films-list.js
@@ -1,19 +1,29 @@
-import {createElement} from '../utils.js';
+import { createElement } from '../utils.js';
+const ALL_MOVIES_EMPTY_MESSAGE = 'There are no movies in our database';
 
-const createFilmsListTemplate = () => (
+const createFilmsListTitle = (moviesLength) => {
+  if (!moviesLength) {
+    const title = ALL_MOVIES_EMPTY_MESSAGE;
+    return `<h2 class="films-list__title">${title}</h2>`;
+  }
+
+  return '<h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>';
+};
+const createFilmsListTemplate = (moviesLength) => (
   `<section class="films-list">
-    <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
+    ${createFilmsListTitle(moviesLength)}
     <div class="films-list__container"></div>
   </section>`
 );
 
 export default class FilmsList {
-  constructor() {
+  constructor(movies) {
     this._element = null;
+    this._moviesLength = movies.length;
   }
 
   getTemplate() {
-    return createFilmsListTemplate();
+    return createFilmsListTemplate(this._moviesLength);
   }
 
   getElement() {
