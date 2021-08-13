c4e3821fd5de308da2e00b1289a9ce93b92e347b
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2021-08-13 16:23:44 +0300

    Добавил новые методы презентеру
    - Метод рендеринга списка фильмов по рейтингу
    - Метод рендеринга списка фильмов по комментариям
    - Метод рендеринга списка фильмов

--- src/presenter/movie.js
+++ src/presenter/movie.js
@@ -10,18 +10,40 @@ import {
   RenderPosition
 } from './utils/render.js';
 
+const MOVIE_COUNT_PER_STEP = 5;
+const EXTRA_FILMS_COUNT = 2;
+const TOP_RATED_LIST_TITLE = 'Top rated';
+const MOST_COMMENTED_LIST_TITLE = 'Most commented';
+
+
 export default class Movie {
-  constructor(filmsContainer, movies) {
+  constructor(filmsContainer) {
     this._filmsContainer = filmsContainer;
-    this._movies = movies;
 
     this._sortComponent = new SortView();
     this._filmsListComponent = new FilmsListView();
     this._filmListEmptyComponent = new FilmsListEmptyView();
-    this._filmsListExtraComponent = new FilmsListExtraView();
+    this._topRatedListComponent = new FilmsListExtraView(TOP_RATED_LIST_TITLE);
+    this._mostCommentedListComponent = new FilmsListExtraView(MOST_COMMENTED_LIST_TITLE);
     this._filmCardComponent = new FilmCardView();
     this._filmPopupComponent = new FilmPoupView();
     this._showMoreButtonComponent = new ShowMoreButtonView();
+
+    this._mainFilmsListElement = this._filmsContainer.querySelector('.films-list');
+    this._mainFilmsListContainerElement = this._filmsContainer.querySelector('.films-list__container');
+    this._topRatedFilmsListContainerElement = this._filmsContainer
+      .querySelector('.films-list:nth-child(2')
+      .querySelector('.films-list__container');
+    this._mostCommentedFilmsListContainerElement = this._filmsContainer
+      .querySelector('.films-list:nth-child(3)')
+      .querySelector('.films-list__container');
+  }
+
+  init(movies, comments) {
+    this._movies = movies.slice();
+    this._comments = comments.slice();
+
+    this._renderFilmList();
   }
 
   _renderSort() {
@@ -38,7 +60,7 @@ export default class Movie {
     // Метод, метод отрисовки попапа
   }
 
-  _renderFilmCards() {
+  _renderFilmCards(from, to, container) {
     // Метод для рендеринга N-карточек фильма за раз
   }
 
@@ -51,16 +73,32 @@ export default class Movie {
     // текущая функция renderTask в main.js
   }
 
-  _renderFilmListExtra() {
-    // Метод для рендеринга доп. списков фильмов (Top rated, Most commented)
+  _renderTopRatedFilmList() {
+    render(this._filmsContainer, this._topRatedListComponent, RenderPosition.BEFOREEND);
+    this._renderFilmCards(0, EXTRA_FILMS_COUNT, this._topRatedFilmsListContainerElement);
+  }
+
+  _renderMostCommentedFilmList() {
+    render(this._filmsContainer, this._mostCommentedListComponent, RenderPosition.BEFOREEND);
+    this._renderFilmCards(0, EXTRA_FILMS_COUNT, this._mostCommentedFilmsListContainerElement);
   }
 
   _renderFilmList() {
-    // Метод для инициализации (начала работы) модуля
-  }
+    if (!this._movies.length) {
+      this._renderFilmListEmpty();
+      return;
+    }
 
-  init(movies) {
-    this.movies = movies.slice();
-    // Метод для инициализации (начала работы) модуля,
+    this._renderSort();
+
+    render(this._filmsContainer, this._filmsListComponent, RenderPosition.BEFOREEND);
+
+    this._renderFilmCards(0, Math.min(this._movies.length, MOVIE_COUNT_PER_STEP), this._mainFilmsListContainerElement);
+    if (this._movies.length > MOVIE_COUNT_PER_STEP) {
+      this._renderShowMoreButton();
+    }
+
+    this._renderTopRatedFilmList();
+    this._renderMostCommentedFilmList();
   }
 }
