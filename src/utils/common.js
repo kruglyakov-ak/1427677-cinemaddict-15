5a2acd60b222da3fff06d4e3da9c5334cf04e7f0
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2021-08-19 19:05:19 +0300

    Реализовал логику обновления карточки фильма

--- src/utils/common.js
+++ src/utils/common.js
@@ -4,3 +4,17 @@ export const getRandomNumberInRange = (min = 0, max = 1, numberSymbolsAfterComma
   const randomNumber = Math.random() * (upper - lower) + lower;
   return +randomNumber.toFixed(numberSymbolsAfterComma);
 };
+
+export const updateItem = (items, update) => {
+  const index = items.findIndex((item) => item.id === update.id);
+
+  if (index === -1) {
+    return items;
+  }
+
+  return [
+    ...items.slice(0, index),
+    update,
+    ...items.slice(index + 1),
+  ];
+};
