bf9e15df4cee79acffd1ca17b82a3a996c0c9428
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2021-09-09 19:49:02 +0300

    Описал провайдер

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,6 +1,1 @@
-export const getRandomNumberInRange = (min = 0, max = 1, numberSymbolsAfterComma = 0) => {
-  const lower = Math.min(Math.abs(min), Math.abs(max));
-  const upper = Math.max(Math.abs(min), Math.abs(max));
-  const randomNumber = Math.random() * (upper - lower) + lower;
-  return +randomNumber.toFixed(numberSymbolsAfterComma);
-};
+export const isOnline = () => window.navigator.onLine;
