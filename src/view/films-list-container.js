b95f1b2442559328ec26dad308f16d68af81802b
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2021-09-10 17:45:02 +0300

    Исправил название класса

--- src/view/films-list-container.js
+++ src/view/films-list-container.js
@@ -2,7 +2,7 @@ import AbstractView from './abstract.js';
 
 const createFilmsListContainerTemplate = () => '<div class="films-list__container"></div>';
 
-export default class FilmsContainer extends AbstractView {
+export default class FilmsListContainer extends AbstractView {
   getTemplate() {
     return createFilmsListContainerTemplate();
   }
